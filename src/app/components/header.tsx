a07e8dc7d66aa9b9425524ceed9b3be8e28feac2
ezzeldeen tantawy <eldeene103@gmail.com>
2025-04-19 21:32:32 +0200

    change the header

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -3,26 +3,9 @@ import Image from "next/image"
 
 export const Header = () => {
     return(
-        <div>
+        <div className="flex flex-col bg-[url('/header/bloom.jpeg')] bg-cover bg-center h-[400px] sm:h-[500px] md:h-[600px] lg:h-[700px]">
             <Navbar />
-            <div className="flex items-center justify-between mx-4 my-4">
-                <div>
-                    <h1 className="text-lg sm:text-2xl md:mdtext-3xl lg:text-5xl font-bold text-orange-500 mt-10">
-                        Welcome to Our Beauty Clinic
-                    </h1>
-                    <p className="text-sm sm:text-md md:text-lg mt-4">
-                        Your journey to beauty and wellness starts here.
-                    </p>
-                </div>
-                <div>
-                    <Image 
-                    src="/header/bloom.jpeg"
-                    className="rounded-2xl w-32 h-32 sm:w-48 sm:h-48"
-                    alt="header image"
-                    width={400}
-                    height={500}
-                    />
-                </div>
+            <div className="flex items-center justify-center my-auto">
             </div>
         </div>
     )
