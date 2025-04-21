a8adb2b35bb2fb9564b19e665deca76f4834a855
ezzeldeen tantawy <eldeene103@gmail.com>
2025-04-21 03:31:20 +0200

    finish the precision diagnostics Lab page

---
+++ src/app/departments/precision-diagnostics-lab/components/header.tsx
@@ -0,0 +1,60 @@
+import Image from "next/image"
+
+export const Header = () => {
+    return(
+        <div className="flex flex-col items-center justify-center text-white px-2 sm:px-6 md:px-10">
+            <div className="flex items-center justify-center">
+                <Image
+                    src="/departments-section/PDL.jpeg"
+                    alt="Precision Diagnostics Lab"
+                    className="rounded-2xl mb-4"
+                    width={500}
+                    height={500}
+                />
+            </div>
+            <div className="flex items-start flex-col">
+                <h1 className="text-xl sm:text-2xl lg:text-3xl text-main-color text-start sm:text-center">
+                    PreCare SmartPlan Diagnostics – Smart pre-diagnostics
+                    for an optimal treatment plan.
+                </h1>
+                <ul className="text-md sm:text-lg md:text-xl mt-4 ps-1 text-start">
+                    <li>&bull; Precision Diagnostics Lab - Smart Diagnostic Room.</li>
+                    <li>&bull; Precision in Diagnosis, Confidence in Treatment.</li>
+                </ul>
+                <p className="text-md sm:text-lg md:text-xl mt-4 px-2 sm:px-4 md:px-6 lg:px-10">
+                    <span className="text-main-color">At Precision Diagnostics Lab</span>, we utilize cutting-edge <span className="underline decoration-1">
+                        AI technology and advanced medical
+                        equipment</span> to provide comprehensive diagnostics and accurate assessments,
+                    helping design personalized treatment plans* tailored to each individual&#39;s unique health needs.
+                </p>
+                <div>
+                    <h2 className="text-xl sm:text-2xl lg:text-3xl text-main-color text-start">
+                        &bull; What does Precision Diagnostics Lab Offer?
+                    </h2>
+                    <ul>
+                        <li className="flex items-center text-lg sm:text-xl md:text-2xl">
+                            <p className="me-4">&bull; Comprehensive medical analysis</p>
+                        </li>
+                        <li className="flex items-center text-lg sm:text-xl md:text-2xl">
+                            <p className="me-4">&bull; Using globally recognized devices
+                                such as <span className="text-main-color">Visia</span>  <span className="underline decoration-1">(USA-Made)</span>,
+                                <span className="text-main-color">Foto finder</span> <span className="underline decoration-1">(Germany-Made)</span>,
+                                and <span className="text-main-color">Oligoscan</span> <span className="underline decoration-1">(France-Made)</span>.</p>
+                        </li>
+                        <li className="flex items-center text-lg sm:text-xl md:text-2xl">
+                            <p className="me-4">&bull; Personalized treatment plans based on precise test
+                                results for optimal health outcomes.</p>
+                        </li>
+                        <li className="flex items-center text-lg sm:text-xl md:text-2xl">
+                            <p className="me-4">&bull; <span className="text-main-color">Unmatched accuracy</span> in diagnostics through AI-powered health assessment.</p>
+                        </li>
+                        <li className="flex items-center text-lg sm:text-xl md:text-2xl">
+                            <p className="me-4">&bull; Scientifically-backed medical recommendations to enhance
+                                well-being and overall health.</p>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
+}
---
+++ src/app/departments/precision-diagnostics-lab/components/tests.tsx
@@ -0,0 +1,66 @@
+
+
+export const Tests = () => {
+    return(
+        <div className="px-2 sm:px-6 md:px-10 text-white">
+            <h1 className="text-main-color text-center text-xl sm:text-2xl lg:text-3xl">
+                Medical Tests available in the Lab
+            </h1>
+            <ul>
+                <li>
+                    <h3 className="text-lg sm:text-xl lg:text-2xl text-main-color">&bull; Visia AI Skin Analysis</h3>
+                    <p className="ps-4 text-md sm:text-lg md:text-xl">AI-powered facial imaging to assess wrinkles, pigmentation, pores, and hydration levels.</p>
+                    <p className="ps-4 text-md sm:text-lg md:text-xl">Helps determine the best skincare treatments, such as chemical peels and laser therapy.</p>
+                </li>
+                <li>
+                    <h3 className="text-lg sm:text-xl lg:text-2xl text-main-color">&bull; Foto Finder AI Hair Analysis</h3>
+                    <p className="ps-4 text-md sm:text-lg md:text-xl">High-precision scan of hair density, follicle health, sebum production, and growth cycle.</p>
+                    <p className="ps-4 text-md sm:text-lg md:text-xl">Provides treatment recommendations like PRP therapy, Mesotherapy, and nutrient-rich oils to restore hair health.</p>
+                </li>
+                <li>
+                    <h3 className="text-lg sm:text-xl lg:text-2xl text-main-color">&bull;  InBody Body Composition Analysis.</h3>
+                    <p className="ps-4 text-md sm:text-lg md:text-xl">Measures body fat percentage, muscle mass, and water levels using bioelectrical impedance technology (BIA).</p>
+                    <p className="ps-4 text-md sm:text-lg md:text-xl">Helps design customized fitness and nutrition programs based on individual health needs.
+                        Oligoscan & FDA-Approved Vitamin Deficiency Test - Mineral & Vitamin Analysis</p>
+                </li>
+                <li><h3 className="text-lg sm:text-xl lg:text-2xl text-main-color">&bull; Oligoscan & FDA-Approved Vitamin Deficiency Test - 
+                    Mineral & Vitamin Analysis</h3>
+                    <p className="ps-4 text-md sm:text-lg md:text-xl">This combined test utilizes advanced medical technology to assess essential mineral and 
+                        {" "}<span className="underline">vitamin levels</span>, 
+                        detecting any nutritional deficiencies or toxic accumulation affecting overall health.
+                        💊 
+                        <span className="underline font-bold">Oligoscan</span> uses spectral scanning to identify mineral imbalances such as 
+                        {" "}<span className="underline font-light">magnesium, calcium, iron, and zinc</span>, as well as 
+                        heavy metal toxicity from mercury and lead, guiding nutritional corrections and detoxification therapies.
+                        💉 <span className="underline">FDA-Approved</span> Vitamin Deficiency Test is a laboratory blood analysis certified by the FDA, measuring Vitamin D, B12, Iron, 
+                        Magnesium, Calcium, and Folic Acid levels to determine precise supplementation and IV therapy solutions such as 
+                        Vitamin B12 Drip for energy support or Mineral Balance Drip for nutritional restoration.</p>
+                    </li>
+                <li>
+                    <h3 className="text-lg sm:text-xl lg:text-2xl text-main-color">&bull; Blood Pressure & Glucose Test.</h3>
+                    <p className="ps-4 text-md sm:text-lg md:text-xl">A rapid test to monitor vital health indicators, ensuring stable glucose 
+                        and blood pressure levels for overall well-being.</p>
+                </li>
+                <li>
+                    <h3 className="text-lg sm:text-xl lg:text-2xl text-main-color">&bull; 3D Digital Dental Scan</h3>
+                    <p className="ps-4 text-md sm:text-lg md:text-xl">High-resolution laser and infrared imaging create a precise digital model of the teeth and jaw.
+                        Helps in planning treatments like orthodontics, teeth whitening, and dental implants for perfect results.  </p>
+                </li>
+                <li>
+                    <h3 className="text-lg sm:text-xl lg:text-2xl text-main-color">&bull; Functional Consultations & Health Tests</h3>
+                    <p className="ps-4 text-md sm:text-lg md:text-xl">Comprehensive health evaluations to determine weak points and 
+                        personalized treatment plans, ensuring targeted healthcare interventions.</p>
+                </li>
+                <li>
+                    <h3 className="text-lg sm:text-xl lg:text-2xl text-main-color">&bull;  DNA Genetic Analysis</h3>
+                    <p className="ps-4 text-md sm:text-lg md:text-xl">Genetic profiling reveals predisposition to diseases, metabolic efficiency, and nutrient absorption.  
+Helps design a tailored lifestyle plan based on genetic markers, optimizing preventive healthcare and nutrition strategies.</p>
+                </li>
+                <li>
+                    <h3 className="text-lg sm:text-xl lg:text-2xl text-main-color">&bull; Food Allergy Test</h3>
+                    <p className="ps-4 text-md sm:text-lg md:text-xl">Detects food sensitivities and intolerances, guiding customized dietary adjustments to minimize inflammation and digestive issues.</p>
+                </li>
+            </ul>
+        </div>
+    )
+}
