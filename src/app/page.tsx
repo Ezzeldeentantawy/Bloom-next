b328727819fb2c68a2eb77b2f98a9abf3df160d1
ezzeldeen tantawy <eldeene103@gmail.com>
2025-04-22 03:02:37 +0200

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,24 +2,19 @@ import { Departments } from "./components/departments";
 import { Header } from "./components/header";
 import Head from "next/head";
 import WhyUs from "./components/whyUs";
-export const metadata = {
-  title: {
-    absolute: 'Best Beauty Clinic in Ras-Elkhaima | Skin, Laser, Botox & More'
-  },
-  description: "Top-rated beauty clinic in UAE offering laser hair removal, hydrafacial, skin rejuvenation, botox, fillers, and more. Book your consultation today!",
-  generator: 'Next.js',
-  applicationName: 'Bloom Beauty Clinic',
-  referrer: 'origin-when-cross-origin',
-  keywords: ["beauty clinic Dubai",
+import { Metadata } from 'next';
+
+export async function generateMetadata(): Promise<Metadata> {
+  return {
+    title: 'About Us | Your Brand',
+    description: 'Learn more about Your Brand, our mission, and our team.',
+    other: {
+      keywords: ["beauty clinic Dubai",
     "laser hair removal UAE",
-    "hydrafacial Abu Dhabi",
-    "botox treatment", "fillers in Dubai", "skin care clinic UAE, aesthetic clinic"],
-  authors: [{ name: 'Ezzeldeen' }, { name: 'Ezzeldeen', url: 'https://www.linkedin.com/in/ezzeldeen-tantawy-8a0393344?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app' }],
-  formatDetection: {
-    email: false,
-    address: false,
-    telephone: false,
-  },
+      "hydrafacial Abu Dhabi",
+      "botox treatment", "fillers in Dubai", "skin care clinic UAE, aesthetic clinic"]
+    }
+  };
 }
 export default function Home() {
   return (
