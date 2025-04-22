92e6c8a656e1720f8a9d6c7064f2a50fc81d1de3
ezzeldeen tantawy <eldeene103@gmail.com>
2025-04-22 02:56:54 +0200

    test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,25 +6,7 @@ const noto = Noto_Serif({
   subsets: ['latin'],
 })
 
-// export const metadata = {
-//   title: {
-//     absolute: 'Best Beauty Clinic in Ras-Elkhaima | Skin, Laser, Botox & More'
-//   },
-//   description: "Top-rated beauty clinic in UAE offering laser hair removal, hydrafacial, skin rejuvenation, botox, fillers, and more. Book your consultation today!",
-//   generator: 'Next.js',
-//   applicationName: 'Bloom Beauty Clinic',
-//   referrer: 'origin-when-cross-origin',
-//   keywords: ["beauty clinic Dubai",
-//     "laser hair removal UAE",
-//     "hydrafacial Abu Dhabi",
-//     "botox treatment", "fillers in Dubai", "skin care clinic UAE, aesthetic clinic"],
-//   authors: [{ name: 'Ezzeldeen' }, { name: 'Ezzeldeen', url: 'https://www.linkedin.com/in/ezzeldeen-tantawy-8a0393344?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app' }],
-//   formatDetection: {
-//     email: false,
-//     address: false,
-//     telephone: false,
-//   },
-// }
+
 
 export default function RootLayout({
   children,
