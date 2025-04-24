e4ec02d807d9e9601c9ff8ec0212dc565ef55b3f
ezzeldeen tantawy <eldeene103@gmail.com>
2025-04-24 03:38:46 +0200

    edit on precision part

--- src/app/departments/dermatology-&-aesthetics/components/precisionPart.tsx
+++ src/app/departments/dermatology-&-aesthetics/components/precisionPart.tsx
@@ -2,7 +2,7 @@ import Link from "next/link"
 import Image from "next/image"
 export const PrecisionDignosticsLab = () => {
     return(
-        <div className="flex sm:flex-col-reverse lg:flex-row items-center justify-between my-4">
+        <div className="flex flex-col-reverse lg:flex-row items-center justify-between my-4">
             <p className="text-md sm:text-lg md:text-xl text-white">
                 Your journey to beauty starts with precision - after selecting your desired 
                 treatment, you&#39;ll first visit our {" "}
