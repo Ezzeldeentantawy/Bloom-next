440e24e6146d40db740fe5f30eaf8c67236782e9
ezzeldeen tantawy <eldeene103@gmail.com>
2025-04-30 07:06:50 +0300

    test

--- src/app/departments/precision-diagnostics-lab/components/header.tsx
+++ src/app/departments/precision-diagnostics-lab/components/header.tsx
@@ -21,7 +21,8 @@ export const Header = () => {
             </div>
             <div className="flex items-start flex-col">
                 <h1 className="text-[28px] sm:text-[30px] md:text-[34px] text-[#ee2424] text-center">
-                    PreCare SmartPlan Diagnostics – Smart pre-diagnostics
+                    PreCare SmartPlan Diagnostics <br/> 
+                    Smart pre-diagnostics
                     for an optimal treatment plan.
                 </h1>
 
